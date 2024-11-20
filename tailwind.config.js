f87c53218d7fe0b0649c99440ee838807612b4a9
malikovshahriyor <malikovshahriyor929@gmail.com>
2024-11-20 21:15:45 +0500

    2pages done

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+primary:['#0058ff'],
+primary2:['#0058ff'],
+      },
+    },
+  },
+  plugins: [],
+}
